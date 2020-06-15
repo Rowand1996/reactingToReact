b2128d53217e55d6f0b2c1661d98b7d3dd603a9f
Rowan Douglas <rowanandvera@gmail.com>
2020-06-15 17:11:09 -0500

    got it mostly working now tying to use hasloaded to render page

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name:"",
+            hasLoaded:false
+        }
+    }
+
+    handleInputChange = (value) => {
+        this.setState({name: value});
+    }
+
+    checkIfLoaded = () => {
+        if(this.state.hasLoaded === false){
+            this.setState({hasLoaded:true});
+            
+        } else {
+            
+            this.setState({hasLoaded:false});
+        }
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <input 
+                placeholder="What's your name?"
+                value={ this.state.name }
+                onChange={ (event) => this.handleInputChange(event.target.value) }
+                />
+                <button onClick = {this.checkIfLoaded}>
+                    click if loaded!
+                </button>
+                <h1>{this.props.text}</h1>
+                <h1>{this.state.name}</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
