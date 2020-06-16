d7031d0da6b610364c78e78f7a615aa4204c610a
Rowan Douglas <rowanandvera@gmail.com>
2020-06-16 09:09:18 -0500

    figured out the last question done with lab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,22 +25,36 @@ class App extends Component {
         }
     }
     
+    componentDidMount = () => {
+        this.setState({hasLoaded:true});
+    }
 
     render() {
-        return (
-            <div>
-                <input 
-                placeholder="What's your name?"
-                value={ this.state.name }
-                onChange={ (event) => this.handleInputChange(event.target.value) }
-                />
-                <button onClick = {this.checkIfLoaded}>
-                    click if loaded!
-                </button>
-                <h1>{this.props.text}</h1>
-                <h1>{this.state.name}</h1>
-            </div>
-        );
+        if(this.state.hasLoaded){
+            return (
+                <div>
+                    <input 
+                    placeholder="What's your name?"
+                    value={ this.state.name }
+                    onChange={ (event) => this.handleInputChange(event.target.value) }
+                    />
+                    <button onClick = {this.checkIfLoaded}>
+                        click if loaded!
+                    </button>
+                    <h1>{this.props.text}</h1>
+                    <h1>{this.state.name}</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h1>Still Loading.....</h1>
+                    <button onClick = {this.checkIfLoaded}>
+                    Click if not loaded!
+                    </button>
+                </div> 
+            );
+        } 
     }
 }
 
